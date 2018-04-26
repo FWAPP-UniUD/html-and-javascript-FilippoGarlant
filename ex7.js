70de2c9792007e2b48ad53360b3050f4628c70f5
FilippoGarlant <filippo.garlant@gmail.com>
2018-04-26 12:14:31 +0200

    wip

--- ex7.js
+++ ex7.js
@@ -4,17 +4,17 @@ document.getElementById("pic").addEventListener("click", onClick);
 document.getElementById("pic").addEventListener("contextmenu", rightclick);
 
 function mouseOver() {
-    alert("You entered the image");
+    window.alert("You entered the image");
 }
 
 function mouseOut() {
-    alert("You exited the image");
+    window.alert("You exited the image");
 }
 
 function onClick() {
-    alert("You clicked the image");
+    window.alert("You clicked the image");
 }
 
 function rightclick() {
-    alert("You right-clicked the image");
+    window.alert("You right-clicked the image");
 }
